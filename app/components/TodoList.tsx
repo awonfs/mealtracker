555d4488a58d0c1c187ba7b9d09986e612287fbc
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2023-08-28 14:24:17 +0300

    Added serverClient to enable SSR for trpc

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,12 +1,36 @@
 "use client";
 import { trpc } from "../_trpc/client";
+import { useState } from "react";
 
 function TodoList() {
   const getTodos = trpc.getTodos.useQuery();
+  const addTodo = trpc.addTodo.useMutation({
+    onSettled: () => {
+      getTodos.refetch();
+    },
+  });
+  const [todo, setTodo] = useState("");
+
+  async function handleTodoSubmit() {
+    if (todo.length === 0) return;
+    await addTodo.mutateAsync(todo);
+    setTodo("");
+  }
   return (
     <div>
       <h1>Todo List</h1>
       <div>{JSON.stringify(getTodos.data)}</div>
+      <div>
+        <label htmlFor="todo">Add todo</label>
+        <input
+          className="text-black"
+          id="todo"
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={handleTodoSubmit}>Add Todo</button>
+      </div>
     </div>
   );
 }
