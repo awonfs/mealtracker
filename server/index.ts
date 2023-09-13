e281ab86e47a12b16fcca169957ba621e9f6744f
Eetu Rönkkö <105866472+awonfs@users.noreply.github.com>
2023-09-13 17:49:40 +0300

    Deleting foodcard that has meals works now

--- server/index.ts
+++ server/index.ts
@@ -11,17 +11,6 @@ const db = drizzle(sqlite);
 migrate(db, { migrationsFolder: "drizzle" });
 
 export const appRouter = router({
-  getTodos: publicProcedure.query(async () => {
-    return await db.select().from(todos).all();
-  }),
-  addTodo: publicProcedure.input(z.string()).mutation(async (options) => {
-    await db.insert(todos).values({ content: options.input, done: 0 }).run();
-    return true;
-  }),
-  deleteTodo: publicProcedure.input(z.number()).mutation(async (options) => {
-    await db.delete(todos).where(eq(todos.id, options.input)).run();
-    return true;
-  }),
   getFoodCards: publicProcedure.query(async () => {
     return await db.select().from(foodCards).all();
   }),
@@ -36,9 +25,13 @@ export const appRouter = router({
     return await db.select().from(foodCards).where(eq(foodCards.id, id.input));
   }),
   deleteFoodCard: publicProcedure.input(z.number()).mutation(async (id) => {
+    //*  First, delete all associated meals with the given foodCardId
+    await db.delete(meals).where(eq(meals.foodCardId, id.input)).run();
+    //* Then delete the foodCard itself
     await db.delete(foodCards).where(eq(foodCards.id, id.input)).run();
     return true;
   }),
+
   addMeal: publicProcedure
     .input(
       z.object({
